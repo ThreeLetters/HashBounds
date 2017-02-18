adff08b9989ab1c22b75a9ed5dd799e19ed4787e
Andrew S <andrews54757@gmail.com>
2017-02-18 11:01:01 -0500

    Update Holder.js

--- Holder.js
+++ Holder.js
@@ -1,30 +1,53 @@
 "use strict"
 
 module.exports = class Holder {
-  constructor(parent) {
-    this.parent = parent;
-    this.map = new Map();
-    this.len = 0;
-    this.children = [];
-  }
-  set(id,node) {
-    
-    this.map.set(id,node)
-      this.add()
-  }
-  add() {
-    ++this.len;
-    if (this.parent) this.parent.add();
-  }
-  sub() {
-    --this.len;
-    if (this.parent) this.parent.sub();
-  }
-  delete(id) {
-   this.map.delete(id) 
-    this.sub()
-  }
-  every(c) {
+    constructor(parent) {
+        this.parent = parent;
+        if (this.parent) this.parent.children.push(this)
+        this.map = new Map();
+        this.len = 0;
+        this.skip = 2;
+        this.id = ~~(Math.random() * 100)
+        this.children = []
+    }
+    set(id, node) {
+
+        this.map.set(id, node)
+        this.add()
+    }
+    add() {
+        ++this.len;
+
+        this.skip = 0;
+
+        if (this.parent) {
+            this.parent.add();
+
+
+        }
+    }
+    toArray() {
+        var nodes = [];
+        this.map.forEach(function (n) {
+            nodes.push(n)
+        })
+        return nodes
+    }
+    sub() {
+        --this.len;
+        if (!this.len) this.skip = 1;
+
+        if (this.parent) {
+            this.parent.sub();
+            this.skip = this.parent.skip * 2;
+
+        }
+    }
+    delete(id) {
+        this.map.delete(id)
+        this.sub()
+    }
+    every(c) {
         var a = this.map.entries()
         var b;
         while (b = a.next().value) {
@@ -32,8 +55,7 @@ module.exports = class Holder {
         }
         return true;
     }
-  forEach(c) {
-    return this.map.forEach(c);
-  }
+    forEach(c) {
+        return this.map.forEach(c);
+    }
 }
-
