ea76a1ae31986c9ad3a1ff2a96d49db7c284df96
Andrew S <andrews54757@gmail.com>
2017-02-18 11:06:02 -0500

    Update HashBounds.js

--- browser/HashBounds.js
+++ browser/HashBounds.js
@@ -1,4 +1,3 @@
-"use strict"
 /*
        HashBounds - A hierarchical spacial hashing system
     Copyright (C) 2016 Andrew S
@@ -16,275 +15,312 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-function Grid(g, p, size,minc) {
-    this.MIN = minc * -1;
-    this.POWER = g;
-    this.LEVEL = p;
-    this.SIZE = size;
-    this.DATA = {};
-    this.LENGTH = 0;
-    this.init()
-}
-Grid.prototype.init = function () {
-    if (this.SIZE >= 65535) {
-        throw "Maximum amount of buckets are 65535^2"
-    } // Max limit is 65535 (16 bits) 
-    // console.log(this.SIZE)
-    for (var j = this.MIN; j <= this.SIZE; ++j) {
-        var x = j << 16
-        for (var i = this.MIN; i <= this.SIZE; ++i) {
+function Holder(parent) {
+        this.parent = parent;
+        if (this.parent) this.parent.children.push(this)
+        this.map = new Map();
+        this.len = 0;
+        this.skip = 2;
+        this.id = ~~(Math.random() * 100)
+        this.children = []
+    }
+    Holder.prototype.set = function(id, node) {
 
-            var key = this._getKey(x, i);
-            // console.log(key)
-            this.DATA[key] = new Map()
+        this.map.set(id, node)
+        this.add()
+    }
+    Holder.prototype.add = function() {
+        ++this.len;
+
+        this.skip = 0;
+
+        if (this.parent) {
+            this.parent.add();
+
 
         }
     }
-
-}
-Grid.prototype._every = function (m, c) {
-    var a = m.entries()
-    var b;
-    while (b = a.next().value) {
-        if (!c(b[1], b[0])) return false;
+    Holder.prototype.toArray = function() {
+        var nodes = [];
+        this.map.forEach(function (n) {
+            nodes.push(n)
+        })
+        return nodes
     }
-    return true;
-}
+    Holder.prototype.sub = function() {
+        --this.len;
+        if (!this.len) this.skip = 1;
 
-Grid.prototype.getKey = function (x, y) {
-    return {
-        x: x >> this.POWER,
-        y: y >> this.POWER
-    }
-}
-Grid.prototype._getKey = function (x, y) {
-    return x | y
+        if (this.parent) {
+            this.parent.sub();
+            this.skip = this.parent.skip * 2;
 
-}
-Grid.prototype.checkBorders = function (x, y) {
-    return !(x > this.SIZE || y > this.SIZE)
-
-}
-Grid.prototype.insert = function (node) {
-
-    //   var a = this.getKey(node.bounds.width, node.bounds.height);
-    // if (a.x + a.y >= 2 && this.LEVEL != 0) return false;
-    this.LENGTH++;
-    var x1 = node.bounds.x,
-        y1 = node.bounds.y,
-        x2 = node.bounds.x + node.bounds.width,
-        y2 = node.bounds.y + node.bounds.height;
-
-    var k1 = this.getKey(x1, y1)
-    var k2 = this.getKey(x2, y2)
-    node.hash.k1 = k1
-    node.hash.k2 = k2
-    node.hash.level = this.LEVEL
-    var lenX = k2.x + 1,
-        lenY = k2.y + 1;
-    for (var j = k1.x; j < lenX; ++j) {
-        var x = j << 16;
-        for (var i = k1.y; i < lenY; ++i) {
-
-            var ke = this._getKey(x, i);
-            // console.log(ke)
-            this.DATA[ke].set(node._HashID, node)
         }
-
     }
-    return true;
-}
-Grid.prototype.delete = function (node) {
-    var k1 = node.hash.k1
-    var k2 = node.hash.k2
-    this.LENGTH--;
-    var lenX = k2.x + 1,
-        lenY = k2.y + 1;
-    for (var j = k1.x; j < lenX; ++j) {
-        var x = j << 16;
-        for (var i = k1.y; i < lenY; ++i) {
-
-
-            var ke = this._getKey(x, i);
-
-            this.DATA[ke].delete(node._HashID)
+    Holder.prototype.delete = function(id) {
+        this.map.delete(id)
+        this.sub()
+    }
+    Holder.prototype.every = function(c) {
+        var a = this.map.entries()
+        var b;
+        while (b = a.next().value) {
+            if (!c(b[1], b[0])) return false;
         }
+        return true;
+    }
+    Holder.prototype.forEach = function(c) {
+        return this.map.forEach(c);
+    }
+function Grid(g, p, size, minc, prev) {
+        this.POWER = g;
+        this.LEVEL = p;
+        this.PREV = prev;
+        this.SIZE = size;
+        this.MIN = minc * -1;
+        this.DATA = {};
+        this.LENGTH = 0;
+        this.init()
+    }
+    Grid.prototype.init = function() {
+        if (this.SIZE >= 65535) {
+            throw "Maximum amount of buckets are 65535^2"
+        } // Max limit is 65535 (16 bits) 
+        // console.log(this.SIZE)
+        for (var j = this.MIN; j <= this.SIZE; ++j) {
+            var x = j << 16
+            var bx = (j >> 1) << 16;
+            for (var i = this.MIN; i <= this.SIZE; ++i) {
+
+                var by = i >> 1
+                var key = this._getKey(x, i);
+
+
+                if (this.PREV) var l = this.PREV.DATA[this._getKey(bx, by)];
+                else var l = false;
+                this.DATA[key] = new Holder(l);
+
+            }
+        }
+    }
+
+    Grid.prototype.getKey = function(x, y) {
+        return {
+            x: x >> this.POWER,
+            y: y >> this.POWER
+        }
+    }
+    Grid.prototype._getKey = function(x, y) {
+        return x | y
 
     }
-}
-Grid.prototype.toArray = function (array, bounds) {
-    if (this.LENGTH <= 0) return;
+   Grid.prototype._get = function(bounds, call) {
+        var x1 = bounds.x,
+            y1 = bounds.y,
+            x2 = bounds.x + bounds.width,
+            y2 = bounds.y + bounds.height;
 
-    var x1 = bounds.x,
-        y1 = bounds.y,
-        x2 = bounds.x + bounds.width,
-        y2 = bounds.y + bounds.height,
-        h = {};
+        var k1 = this.getKey(x1, y1)
+        var k2 = this.getKey(x2, y2)
 
-    var k1 = this.getKey(x1, y1)
-    var k2 = this.getKey(x2, y2)
-    for (var j = k1.x; j <= k2.x; ++j) {
-        var x = j << 16;
-        for (var i = k1.y; i <= k2.y; ++i) {
+        for (var j = k1.x; j <= k2.x; ++j) {
 
-            var ke = this._getKey(x, i);
+            var x = j << 16;
 
-            if (this.DATA[ke]) this.DATA[ke].forEach((node, i) => {
+            for (var i = k1.y; i <= k2.y; ++i) {
 
-                if (h[i]) return;
-                h[i] = true;
-                array.push(node)
+
+                var key = this._getKey(x, i);
+                if (this.DATA[key]) {
+
+                    if (this.DATA[key].skip) {
+
+                        i += this.DATA[key].skip - 1;
+                        console.log(this.DATA[key].skip)
+                    } else {
+
+                        if (!call(this.DATA[key])) return false
+                    }
+                }
+
+            }
+        }
+        return true;
+    }
+
+    Grid.prototype.insert = function(node) {
+
+        //   var a = this.getKey(node.bounds.width, node.bounds.height);
+        // if (a.x + a.y >= 2 && this.LEVEL != 0) return false;
+        this.LENGTH++;
+        var x1 = node.bounds.x,
+            y1 = node.bounds.y,
+            x2 = node.bounds.x + node.bounds.width,
+            y2 = node.bounds.y + node.bounds.height;
+
+        var k1 = this.getKey(x1, y1)
+        var k2 = this.getKey(x2, y2)
+        node.hash.k1 = k1
+        node.hash.k2 = k2
+        node.hash.level = this.LEVEL
+        var lenX = k2.x + 1,
+            lenY = k2.y + 1;
+        for (var j = k1.x; j < lenX; ++j) {
+            var x = j << 16;
+            for (var i = k1.y; i < lenY; ++i) {
+
+                var ke = this._getKey(x, i);
+                // console.log(ke)
+                this.DATA[ke].set(node._HashID, node)
+            }
+
+        }
+        return true;
+    }
+    Grid.prototype.delete = function(node) {
+        var k1 = node.hash.k1
+        var k2 = node.hash.k2
+        this.LENGTH--;
+        var lenX = k2.x + 1,
+            lenY = k2.y + 1;
+        for (var j = k1.x; j < lenX; ++j) {
+            var x = j << 16;
+            for (var i = k1.y; i < lenY; ++i) {
+
+
+                var ke = this._getKey(x, i);
+
+                this.DATA[ke].delete(node._HashID)
+            }
+
+        }
+    }
+    Grid.prototype.toArray = function(array, bounds) {
+        if (this.LENGTH <= 0) return;
+        var hsh = {};
+
+        this._get(bounds, function (cell) {
+
+            cell.forEach(function (obj, i) {
+                if (hsh[i]) return
+                hsh[i] = true;
+                array.push(obj);
+
             })
+            return true;
+        })
+    }
+    Grid.prototype.every = function(bounds, call) {
+        if (this.LENGTH <= 0) return;
+        var hsh = {};
+
+        this._get(bounds, function (cell) {
+
+            return cell.every(function (obj, i) {
+                if (hsh[i]) return true;
+                hsh[i] = true;
+                return call(obj);
+
+            })
+        })
+    }
+    Grid.prototype.forEach = function(bounds, call) {
+
+        if (this.LENGTH <= 0) return;
+        var hsh = {};
+
+        this._get(bounds, function (cell) {
+
+            cell.every(function (obj, i) {
+                if (hsh[i]) return;
+                hsh[i] = true;
+                call(obj);
+
+            })
+            return true;
+        })
+    }
+function HashBounds(power, lvl, max, minc) {
+
+        this.INITIAL = power;
+        this.LVL = lvl;
+        this.MAX = max;
+        this.MINC = minc || 0;
+        this.MIN = power + 1;
+        this.LEVELS = []
+        this.lastid = 0;
+        this.createLevels()
+        this.SQRT = [];
+        this.setupSQRT()
+    }
+    HashBounds.prototype.setupSQRT = function() {
+        for (var i = 0; i < 255; ++i) {
+            this.SQRT.push(Math.floor(Math.sqrt(i)))
+        }
+    }
+    HashBounds.prototype.createLevels = function() {
+        this.LEVELS = [];
+
+        var last = false
+        for (var i = this.LVL - 1; i >= 0; --i) {
+            var a = this.INITIAL + i;
+
+            var grid = new Grid(a, i, this.MAX >> a, this.MINC >> a, last)
+            this.LEVELS[i] = grid;
+            last = grid;
         }
 
     }
-}
-Grid.prototype.every = function (bounds, call) {
-    if (this.LENGTH <= 0) return true;
-    var x1 = bounds.x,
-        y1 = bounds.y,
-        x2 = bounds.x + bounds.width,
-        y2 = bounds.y + bounds.height,
-        h = {};
-
-    var k1 = this.getKey(x1, y1)
-    var k2 = this.getKey(x2, y2)
-    for (var j = k1.x; j <= k2.x; ++j) {
-        var x = j << 16;
-        for (var i = k1.y; i <= k2.y; ++i) {
-
-            var ke = this._getKey(x, i);
-
-            if (this.DATA[ke])
-                if (!this._every(this.DATA[ke], (a, b) => {
-
-                        if (h[b]) return true;
-                        h[b] = true;
-                        return call(a, b);
-                    })) return false;
+    HashBounds.prototype.clear = function() {
+        this.createLevels();
+    }
+    HashBounds.prototype.update = function(node) {
+        this.delete(node)
+        this.insert(node)
+    }
+    HashBounds.prototype.insert = function(node) {
+        if (node.hash) throw "ERR: A node cannot be already in a hash!"
+        var bounds = node.bounds;
+        node.hash = {}
+        if (!node._HashID) node._HashID = ++this.lastid;
+        if (node._HashSize == node.bounds.width + node.bounds.height) {
+            this.LEVELS[node._HashIndex].insert(node);
+            return;
         }
 
+        var index = this.SQRT[(node.bounds.width + node.bounds.height) >> this.MIN]
+        if (index >= this.LVL) index = this.LVL - 1;
+
+        node._HashIndex = index;
+        node._HashSize = node.bounds.width + node.bounds.height;
+        this.LEVELS[index].insert(node);
+        //for (var i = 0; i < len; ++i) {
+        //   if (this.LEVELS[len - i - 1].insert(node)) break;
+        //}
     }
-    return true;
-}
-Grid.prototype.forEach = function (bounds, call) {
-    if (this.LENGTH <= 0) return;
-    var x1 = bounds.x,
-        y1 = bounds.y,
-        x2 = bounds.x + bounds.width,
-        y2 = bounds.y + bounds.height,
-        h = {};
 
-    var k1 = this.getKey(x1, y1)
-    var k2 = this.getKey(x2, y2)
 
-    for (var j = k1.x; j <= k2.x; ++j) {
-        var x = j << 16;
-        for (var i = k1.y; i <= k2.y; ++i) {
-
-            var ke = this._getKey(x, i);
-
-            if (this.DATA[ke])
-                this.DATA[ke].forEach((a, b) => {
-                    if (h[b]) return;
-                    h[b] = true;
-                    call(a, b)
-                })
+    HashBounds.prototype.delete = function(node) {
+        if (!node.hash) throw "ERR: Node is not in a hash!"
+        this.LEVELS[node.hash.level].delete(node)
+        node.hash = null;
+    }
+    HashBounds.prototype.toArray = function(bounds) {
+        var array = [];
+        for (var i = 0; i < this.LEVELS.length; i++) {
+            this.LEVELS[i].toArray(array, bounds)
         }
-
+        return array;
+    }
+    HashBounds.prototype.every = function(bounds, call) {
+        for (var i = 0; i < this.LEVELS.length; i++) {
+            if (!this.LEVELS[i].every(bounds, call)) return false;
+        }
+        return true;
+    }
+    HashBounds.prototype.forEach = function(bounds, call) {
+        for (var i = 0; i < this.LEVELS.length; i++) {
+            this.LEVELS[i].forEach(bounds, call)
+        }
     }
 
-}
-
-function HashBounds(power, lvl, max,minc) {
-    this.INITIAL = power;
-    this.LVL = lvl;
-this.MINC = minc;
-    this.MAX = max;
-    this.MIN = power + 1;
-    this.LEVELS = []
-    this.lastid = 0;
-    this.createLevels()
-    this.SQRT = [];
-    this.setupSQRT()
-}
-
-HashBounds.prototype.setupSQRT = function () {
-    for (var i = 0; i < 255; ++i) {
-        this.SQRT.push(Math.floor(Math.sqrt(i)))
-    }
-}
-
-HashBounds.prototype.createLevels = function () {
-    this.LEVELS = [];
-    var a = this.INITIAL;
-    for (var i = 0; i < this.LVL; i++, a++) {
-
-        this.LEVELS.push(new Grid(a, i, this.MAX >> a,this.MINC >> a))
-    }
-}
-HashBounds.prototype.clear = function () {
-    this.createLevels();
-}
-HashBounds.prototype.update = function (node) {
-    this.delete(node)
-    this.insert(node)
-}
-HashBounds.prototype.insert = function (node) {
-    if (node.hash) throw "ERR: A node cannot be already in a hash!"
-    var bounds = node.bounds;
-    node.hash = {}
-    if (!node._HashID) node._HashID = ++this.lastid;
-    if (node._HashSize == node.bounds.width + node.bounds.height) {
-        this.LEVELS[node._HashIndex].insert(node);
-        return;
-    }
-
-    var index = this.SQRT[(node.bounds.width + node.bounds.height) >> this.MIN]
-    if (index >= this.LVL) index = this.LVL - 1;
-
-    node._HashIndex = index;
-    node._HashSize = node.bounds.width + node.bounds.height;
-    this.LEVELS[index].insert(node);
-    //for (var i = 0; i < len; ++i) {
-    //   if (this.LEVELS[len - i - 1].insert(node)) break;
-    //}
-}
-
-
-HashBounds.prototype.delete = function (node) {
-    if (!node.hash) throw "ERR: Node is not in a hash!"
-    this.LEVELS[node.hash.level].delete(node)
-    node.hash = null;
-}
-HashBounds.prototype.toArray = function (bounds) {
-    var array = [];
-    for (var i = 0; i < this.LEVELS.length; i++) {
-        this.LEVELS[i].toArray(array, bounds)
-    }
-    return array;
-}
-HashBounds.prototype.every = function (bounds, call) {
-    for (var i = 0; i < this.LEVELS.length; i++) {
-        if (!this.LEVELS[i].every(bounds, call)) return false;
-    }
-    return true;
-}
-HashBounds.prototype.forEach = function (bounds, call) {
-    for (var i = 0; i < this.LEVELS.length; i++) {
-        this.LEVELS[i].forEach(bounds, call)
-    }
-}
-
-
-if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-    module.exports = HashBounds;
-} else {
-    if (typeof define === 'function' && define.amd) {
-        define([], function () {
-            return HashBounds;
-        });
-    } else {
-        window.HashBounds = HashBounds;
-    }
-}
