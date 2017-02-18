d29986283868de235e8bec52974f7728fe808289
Andrew S <andrews54757@gmail.com>
2017-02-17 23:59:10 -0500

    Update Holder.js

--- Holder.js
+++ Holder.js
@@ -1,1 +1,27 @@
+"use strict"
+
+module.exports = class Holder {
+  constructor(parent) {
+    this.parent = parent;
+    this.map = new Map();
+    this.children = [];
+  }
+  set(id,node) {
+    this.map.set(id,node)
+  }
+  delete(id) {
+   this.map.delete(id) 
+  }
+  every(c) {
+        var a = this.map.entries()
+        var b;
+        while (b = a.next().value) {
+            if (!c(b[1], b[0])) return false;
+        }
+        return true;
+    }
+  forEach(c) {
+    return this.map.forEach(c);
+  }
+}
 
