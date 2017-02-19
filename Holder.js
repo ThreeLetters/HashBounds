023c57f390b18be16075a995ca131dc361ceef95
Andrew S <andrews54757@gmail.com>
2017-02-18 22:10:52 -0500

    Update Holder.js

--- Holder.js
+++ Holder.js
@@ -1,60 +1,79 @@
 "use strict"
 
 module.exports = class Holder {
-    constructor(parent, i) {
-        this.parent = parent;
-        if (this.parent) this.parent.children.push(this)
-        this.map = new Map();
-        this.len = 0;
-        this.i = i;
-        this.skip = 1;
-        this.id = ~~(Math.random() * 100)
-        this.children = []
-        this.start = this.i;
+    constructor(parent, x,y,power,lvl) {
+        this.PARENT = parent;
+        if (this.PARENT) this.PARENT.CHILDREN.push(this)
+        this.MAP = new Map();
+        this.POWER = power;
+        this.LVL = lvl
+        this.LEN = 0;
+        this.X = x;
+        this.Y = y;
+      this.BOUNDS = {
+       x: x << power,
+       y: y << power,
+       width: 2 << power,
+       height: 2 << power
+      }
+        this.CHILDREN = []
+      
     }
+    checkIntersect(r1,r2) {
+     var mx1 = r1.x + r1.width,
+         mx2 = r2.x + r2.width,
+         my1 = r1.y + r1.height,
+         my2 = r2.y + r2.height;
+        /*
+        !(r2.left > r1.right || 
+           r2.right < r1.left || 
+           r2.top > r1.bottom ||
+           r2.bottom < r1.top);
+        
+        */
+        
+        
+        
+     return !(r2.x >= mx1 || mx2 <= r1.x || r2.y >= my1 || my2 <= r1.y)
+     
+    }
+    
     set(id, node) {
 
-        this.map.set(id, node)
+        this.MAP.set(id, node)
         this.add()
     }
     add() {
         ++this.len;
 
-        this.skip = 0;
+       
 
-        if (this.parent) {
-            this.parent.add();
+        if (this.PARENT) {
+            this.PARENT.add();
 
 
         }
     }
     toArray() {
         var nodes = [];
-        this.map.forEach(function (n) {
+        this.PARENT.forEach(function (n) {
             nodes.push(n)
         })
         return nodes
     }
     sub() {
         --this.len;
-        if (!this.len) {
-            this.skip = 1;
-            this.start = this.i;
-        }
-        if (this.parent) {
-            this.parent.sub();
-            if (this.parent.skip) {
-                this.skip = this.parent.skip << 1;
-                this.start = this.parent.start << 1;
-            }
+        if (this.PARENT) {
+            this.PARENT.sub();
+            
         }
     }
     delete(id) {
-        this.map.delete(id)
+        this.PARENT.delete(id)
         this.sub()
     }
     every(c) {
-        var a = this.map.entries()
+        var a = this.MAP.entries()
         var b;
         while (b = a.next().value) {
             if (!c(b[1], b[0])) return false;
@@ -62,6 +81,7 @@ module.exports = class Holder {
         return true;
     }
     forEach(c) {
-        return this.map.forEach(c);
+        return this.MAP.forEach(c);
     }
+    
 }
